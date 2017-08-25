d6bef0135ee9160e40734b138fae216362c0c481
Lyddem <lydiademi@gmail.com>
2017-08-24 20:37:57 -0700

    Completed

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-  };
-
+    var node = new Node(value);
+      if(list.tail === null){
+        list.head = node;
+        list.tail = node;
+        //list.head;
+      } else {
+     console.log(list.head);
+     list.tail.next = node;
+     list.tail = node;
+     console.log(list.head);
+      }
+};
   list.removeHead = function() {
+    var currentNode = list.head;
+    list.head = currentNode.next;
+    return currentNode.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      console.log(currentNode);
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
@@ -23,7 +45,5 @@ var Node = function(value) {
 
   return node;
 };
+// Complexity
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
