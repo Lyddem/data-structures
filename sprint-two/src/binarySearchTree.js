bcdd4f5a44701a38df5966e0e9cb61223b7c5c4a
Lyddem <lydiademi@gmail.com>
2017-08-25 21:57:46 -0700

    First commit

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,60 @@
 var BinarySearchTree = function(value) {
+
+var binaryTree = {};
+
+binaryTree.root = new treeNode(value);
+
+_.extend(binaryTree,biTreeMethods);
+return binaryTree;
+}
+
+var biTreeMethods = {};
+biTreeMethods.insert = function (value) {
+   var newNode = new treeNode(value);
+    if (this.root === undefined) {
+      this.root = newNode;
+    } else {
+      var currentNode = this.root //5
+        while (currentNode !== null) {
+          if (currentNode.value > value) {
+              if(currentNode.left === null) {
+               currentNode.left = newNode;
+                break;
+              }
+             currentNode = currentNode.left;  // undefined
+          } else {
+            if(currentNode.right === null) {
+               currentNode.right = newNode;
+                break;
+              }
+            currentNode = currentNode.right;
+          }
+        }
+      }
 };
 
+
+biTreeMethods.contains = function() {
+};
+
+biTreeMethods.depthFirstLog = function() {
+};
+
+
+  function treeNode(val){
+    var  node ={}
+    node.value = val;
+    node.left = null;
+    node.right = null;
+    return node
+  }
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
+
+
+
